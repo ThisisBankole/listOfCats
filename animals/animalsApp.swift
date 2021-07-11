33a95a3773f21abce25039a0c25b98913c68c1c0
Bankole Jamgbadi <bjamgbadi@gmail.com>
2021-07-11 19:22:40 +0100

    Initial Commit

---
+++ animals/animalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  animalsApp.swift
+//  animals
+//
+//  Created by user on 2021-07-11.
+//
+
+import SwiftUI
+
+@main
+struct animalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
